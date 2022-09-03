0eec3915a2a903ff057a8085b6ad832a7d71d240
Henri <henri.hamunen@gmail.com>
2022-09-03 21:28:10 +0300

    ex 6.3 - voting anecdotes

--- src/reducers/anecdoteReducer.js
+++ src/reducers/anecdoteReducer.js
@@ -23,7 +23,25 @@ const reducer = (state = initialState, action) => {
   console.log('state now: ', state)
   console.log('action', action)
 
-  return state
+  switch (action.type) {
+    case 'VOTE':
+      const id = action.data.id
+      const anecdoteToChange = state.find(a => a.id === id)
+      const changedAnecdote = {
+        ...anecdoteToChange,
+        votes: anecdoteToChange.votes + 1
+      }
+      return state.map(a => a.id !== id ? a : changedAnecdote)
+    default:
+      return state
+  }
+}
+
+export const voteAnecdote = (id) => {
+  return {
+    type: 'VOTE',
+    data: { id }
+  }
 }
 
 export default reducer
--- src/reducers/anecdoteReducer.test.js
+++ src/reducers/anecdoteReducer.test.js
@@ -1,6 +1,24 @@
-import deepFreeze from 'deep-freeze'
 import anecdoteReducer from './anecdoteReducer'
+import deepFreeze from 'deep-freeze'
+
 
 describe('anecdote reducer', () => {
+  const initialState = anecdoteReducer(undefined, {type: 'DO_NOTHING'})
 
+  test('Voting increments votes', () => {
+
+    const action = {
+      type: 'VOTE',
+      data: { id: initialState[0].id }
+    }
+    deepFreeze(initialState)
+    //console.log(initialState)
+
+    let newState = anecdoteReducer(initialState, action)
+    newState = anecdoteReducer(newState, action)
+    newState = anecdoteReducer(newState, action)
+    expect(newState[0].votes).toBe(3)
+  })
+
+  
 })
