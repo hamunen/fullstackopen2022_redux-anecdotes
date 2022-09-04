a3a46228f5cd3746609cf98644682785d88d7fae
Henri <henri.hamunen@gmail.com>
2022-09-04 15:34:25 +0300

    Update tests

--- src/reducers/anecdoteReducer.test.js
+++ src/reducers/anecdoteReducer.test.js
@@ -5,19 +5,43 @@ import deepFreeze from 'deep-freeze'
 describe('anecdote reducer', () => {
   const initialState = anecdoteReducer(undefined, {type: 'DO_NOTHING'})
 
+  test('Create anecdote', () => {
+    const state = []
+    const action = {
+      type: 'anecdotes/createAnecdote',
+      payload: 'Test anecdote 1'
+    }
+
+    deepFreeze(state)
+    const newState = anecdoteReducer(state, action)
+
+    expect(newState).toHaveLength(1)
+    expect(newState.map(s => s.content)).toContainEqual(action.payload)
+  })
+
   test('Voting increments votes', () => {
 
-    const action = {
-      type: 'VOTE',
-      data: { id: initialState[0].id }
-    }
-    deepFreeze(initialState)
-    //console.log(initialState)
+    const state = [ 
+      { content: 'Nothing should happen here', id: 0, votes: 1 },
+      { content: 'Anecdote to vote', id: 1, votes: 3 }
+    ]
 
-    let newState = anecdoteReducer(initialState, action)
+    const action = {
+      type: 'anecdotes/voteAnecdote',
+      payload: 1 }
+
+    deepFreeze(state)
+
+    let newState = anecdoteReducer(state, action)
     newState = anecdoteReducer(newState, action)
     newState = anecdoteReducer(newState, action)
-    expect(newState[0].votes).toBe(3)
+
+    expect(newState).toHaveLength(2)
+    expect(newState).toContainEqual(state[0])
+    expect(newState).toContainEqual(
+      {content: 'Anecdote to vote', id: 1, votes: 6 }
+    )
+    
   })
 
   
